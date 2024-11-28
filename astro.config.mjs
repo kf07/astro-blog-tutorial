cab7e0423289458b424b016b9bb499625c7bcd4f
kango furusu <furusu0715@gmail.com>
2024-11-28 23:25:45 +0900

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	output: 'server',
-	adapter: vercel(),
-	integrations: [],
+    site: 'https://example.com',
+    output: 'server',
+    adapter: vercel(),
+    integrations: [svelte()],
 });
