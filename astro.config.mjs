2130bd7b0f43cd3afd9d1347d7e84070255078ef
kango furusu <furusu0715@gmail.com>
2024-12-01 15:28:13 +0900

    vercelStaticに変更 検索を戻す

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import pagefind from "astro-pagefind";
-import vercel from '@astrojs/vercel/serverless';
+import vercelStatic from '@astrojs/vercel/static';
 
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    output: 'server',
-    adapter: vercel(),
+	output: 'static',
+	adapter: vercelStatic(),
     integrations: [
 		svelte(),
 		pagefind()
